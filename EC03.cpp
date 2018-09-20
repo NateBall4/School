445286d3a9020ca3a4be215a5cf92af9b0de124d
NateBall4 <43415745+NateBall4@users.noreply.github.com>
2018-09-20 09:45:46 -0400

    Update EC03.cpp

--- EC03.cpp
+++ EC03.cpp
@@ -1,7 +1,7 @@
 // EC03.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <stdio.h>
 #include <string.h>
 #include <conio.h>
 
@@ -10,36 +10,36 @@
 
 int main()
 {
-	char list[5] = " "; 
+	char list[5] = " ";
 	char phantom;
 
 	printf("Please enter a single letter: ");
 	scanf("%c%c", &list[0], &phantom);
 
-		printf("\nPlease enter a single letter: ");
-		scanf("%c", &list[1]);
-		rewind(stdin);
-		
-		printf("\nPlease enter a single letter: ");
-		scanf("%c", &list[2]);
-		rewind(stdin);
+	printf("\nPlease enter a single letter: ");
+	scanf("%c", &list[1]);
+	rewind(stdin);
 
-		printf("\nPlease enter a single letter: ");
-		scanf("%c", &list[3]);
-		rewind(stdin);
+	printf("\nPlease enter a single letter: ");
+	scanf("%c", &list[2]);
+	rewind(stdin);
 
-		printf("\nPlease enter a single letter: ");
-		scanf("%c", &list[4]);
+	printf("\nPlease enter a single letter: ");
+	scanf("%c", &list[3]);
+	rewind(stdin);
 
-		printf("\nLetter    Number    ASCII\n");
+	printf("\nPlease enter a single letter: ");
+	scanf("%c", &list[4]);
 
-		    printf("%c         %d        %x\n", list[0], list[0], list[0]);
-			printf("%c         %d        %x\n", list[1], list[1], list[1]);
-			printf("%c         %d        %x\n", list[2], list[2], list [2]);
-			printf("%c         %d       %x\n", list[3], list[3], list[3]);
-			printf("%c         %d       %x\n", list[4], list[4], list[4]);
+	printf("\nASCII  |  DEC  |  ADDRESS\n");
+
+	printf("%-6c | %-6d| %x\n", list[0], list[0], &list[0]);
+	printf("%-6c | %-6d| %x\n", list[1], list[1], &list[1]);
+	printf("%-6c | %-6d| %x\n", list[2], list[2], &list[2]);
+	printf("%-6c | %-6d| %x\n", list[3], list[3], &list[3]);
+	printf("%-6c | %-6d| %x\n", list[4], list[4], &list[4]);
 
 
 	_getch();
-    return 0;
+	return 0;
 }

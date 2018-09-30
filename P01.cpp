8bc8e03f0ebf4c8c37b39afb212256c30a1246b2
NateBall4 <43415745+NateBall4@users.noreply.github.com>
2018-09-29 21:38:24 -0400

    Update P01.cpp

--- P01.cpp
+++ P01.cpp
@@ -3,7 +3,6 @@
 #include <conio.h>
 
 int main() {
-	//variables
 	int a = 0;
 	int b = 0;
 	char choice = ' ';
@@ -12,19 +11,14 @@ int main() {
 	int valid = 0;
 	double answer = 0.0;
 
-
-	//get input for value a
 	printf("Please enter your first value: ");
 	scanf("%d", &a);
 	rewind(stdin);
 
-	//get input for value b
 	printf("\nPlease enter your second value: ");
 	scanf("%d", &b);
 	rewind(stdin);
 
-
-	//checks both values for bounds
 	if (a > 1000 || a < -1000) {
 		a = invalid;
 	}
@@ -34,10 +28,8 @@ int main() {
 
 	printf("\n\n");
 
-	//while values are out of bounds loop
 	while (a == invalid || b == invalid) {
-		
-		//gets new input for value a 
+
 		if (a == invalid) {
 			printf("the first value you entred is out of bounds\n");
 
@@ -47,7 +39,6 @@ int main() {
 			printf("\n\n");
 		}
 
-		//gets new input for value b
 		if (b == invalid) {
 			printf("the second value you entred is out of bounds\n");
 
@@ -56,8 +47,8 @@ int main() {
 			rewind(stdin);
 			printf("\n\n");
 		}
-		
-	//checks to see if new values are in bounds
+
+
 		if (a > 1000 || a < -1000) {
 			a = invalid;
 		}
@@ -68,8 +59,6 @@ int main() {
 
 
 	}
-
-	//list of functions
 	printf("What calculation would you Like to perform?\n");
 	printf("For addition enter: +\n");
 	printf("For subtraction enter: -\n");
@@ -77,29 +66,25 @@ int main() {
 	printf("For division enter: /\n");
 	printf("For modulus enter: %%\n\n");
 
-	//gets input for choice
+
 	printf("Please enter your choice: ");
 	scanf("%c", &choice);
 	rewind(stdin);
 	printf("\n");
 
-
-	//checks to see if use is dividing by zero
 	if (b == 0 && choice == '/' || b == 0 && choice == '%') {
 		choice = 'E';
 		printf("Cannot divide by 0\n");
-	} // checks to see if user input a valid function
+	}
 	else if (choice == '+' || choice == '-' || choice == '*' || choice == '/' || choice == '%' && choice != invalid) {
-		//if input is valid it stores that value in holder and marks the choice as valid
 		holder = choice;
 		choice = valid;
 
 	}
-	else {//markes input as invalid ('I') to initiate the while loop
-		printf("----------------------------------------------------------------\n");
+	else {
 		printf("That is not a valid input\n\n");
 		choice = 'I';
-		//while loop for invalid input
+
 		while (choice = 'I') {
 			printf("What calculation would you Like to perform?\n");
 			printf("For addition enter: +\n");
@@ -108,13 +93,12 @@ int main() {
 			printf("For division enter: /\n");
 			printf("For modulus enter: %%\n\n");
 
-			//gets new input
+
 			printf("Please enter your choice: ");
 			scanf("%c", &choice);
 			rewind(stdin);
 			printf("\n");
 
-			//repeats if else loop from before
 			if (b == 0 && choice == '/' || b == 0 && choice == '%') {
 				choice = 'E';
 				printf("Cannot divide by 0\n");
@@ -125,7 +109,7 @@ int main() {
 				choice = valid;
 				break;
 			}
-			else {//if new input is invalid it marks it again and while loop repeats
+			else {
 				printf("----------------------------------------------------------------\n");
 				printf("That is not a valid input\n\n");
 				choice = 'I';
@@ -135,7 +119,7 @@ int main() {
 	}
 
 
-	//formulas for various functions
+
 	switch (holder) {
 
 	case '+':
@@ -169,3 +153,14 @@ int main() {
 	_getch();
 	return 0;
 }
+
+//A) 9 % 2 = 1.0000
+//B) -9 % 2 = -1.0000
+//C) 9 % -2 = 1.0000
+//D) -9 % -2 = -1.0000
+//
+//The modulus function finds the remainder after dividing the two functions for instance 
+//5 % 10 is 0 because 5/2 = 0 and there are no decimal places
+//if the remainder is on the negative spectrum it is considered a negative number. however,
+//if it is on the positive spectrum it is a positive number.
+//In the instance of 9 % -2 the remainder was positive because the first number was positive.
